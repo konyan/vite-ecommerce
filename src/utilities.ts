dcc03f8c47ee57ba484d62218947a1a95403aa03
ko nyan <konyan@kos-MacBook-Pro.local>
2022-10-29 14:56:12 +0700

    add validation for cardnumber and card type and expire date and cvv and pay api integration

---
+++ src/utilities.ts
@@ -0,0 +1,38 @@
+export interface TypeAppTitle {
+    title?: string
+    suffix?: string
+    addSuffix?: boolean
+    returnAppTitle?: boolean
+}
+
+export const appTitle = `${import.meta.env.VITE_APP_TITLE}`
+
+export const getAppTitle = (options: TypeAppTitle = {}): string => {
+    const {
+        title,
+        suffix = appTitle,
+        addSuffix = true,
+        returnAppTitle = true,
+    } = options
+
+    if (title === undefined && returnAppTitle) {
+        return appTitle
+    }
+
+    if (addSuffix) {
+        return `${title} - ${suffix}`
+    }
+
+    return title || ""
+}
+
+export const wait = (ms: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
+
+//generate random number
+export const generateRandomNumber = () => {
+    return Math.floor(Math.random() * (9999999 - 1111111 + 1)) + 1111111
+}
